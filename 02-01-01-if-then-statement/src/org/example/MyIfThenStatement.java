c0ed31007a6e0e6a327a9c5d7ca93ff3d4428463
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-19 12:55:28 +0100

    E1.03 Bekérjük a számot.

--- 02-01-01-if-then-statement/src/org/example/MyIfThenStatement.java
+++ 02-01-01-if-then-statement/src/org/example/MyIfThenStatement.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class MyIfThenStatement {
 
 	public static void main(String[] args) {
@@ -9,6 +11,14 @@ public class MyIfThenStatement {
 		
 		// E1.02 A feladatot kisebb részekre (szekvenciákra) bontjuk.
 		//		 Megfogalmazzuk saját szavainkkal a kisebb részek algoritmusát. Ezt leprogramozzuk.
+
+		// E1.03 Bekérjük a számot.		
+		System.out.println("Megállapítom, hogy a szám páros-e!");
+		System.out.print("Adja meg a számot: ");
+		Scanner input=new Scanner(System.in);
+		int number;
+		number=input.nextInt();
+
 	}
 
 }
