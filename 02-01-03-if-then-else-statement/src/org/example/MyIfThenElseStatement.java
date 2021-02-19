ecb6383927c715e742e4326744b245fde9ff2c17
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-19 13:07:21 +0100

    E3.02 Bekérjük a számot

--- 02-01-03-if-then-else-statement/src/org/example/MyIfThenElseStatement.java
+++ 02-01-03-if-then-else-statement/src/org/example/MyIfThenElseStatement.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class MyIfThenElseStatement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		// E3.01 Írjunk programot, amely megállpítja, hogy a szám páros vagy páratlan!
+	
+		// E3.02 Bekérjük a számot
+		System.out.println("Megállapítom, hogy a szám páros-e vagy páratlan!");
+		System.out.print("Adja meg a számot: ");
+		Scanner input=new Scanner(System.in);
+		int number;
+		number=input.nextInt();
 	}
 
 }
