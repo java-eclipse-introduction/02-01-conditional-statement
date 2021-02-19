cee5ed6652e736fd5d6eabc67af5ad21a86d1d5f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-19 13:16:52 +0100

    E4.01 Írjon programot, amely megállapítja, hogy a szám pozitív, negatív vagy nulla

--- 02-01-04-else-if-statement/src/org/example/MyElseIfStatemanet.java
+++ 02-01-04-else-if-statement/src/org/example/MyElseIfStatemanet.java
@@ -5,6 +5,7 @@ public class MyElseIfStatemanet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// E4.01 Írjon programot, amely megállapítja, hogy a szám pozitív, negatív vagy nulla
 	}
 
 }
