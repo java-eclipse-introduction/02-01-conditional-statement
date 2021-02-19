d42a9f5a765a12586c7d49f429925d235ead3cb7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-19 13:00:10 +0100

    E2.01 Boolean változó használata

--- 02-01-02-if-then-boolean/src/org/example/MyDebug.java
+++ 02-01-02-if-then-boolean/src/org/example/MyDebug.java
@@ -4,7 +4,8 @@ public class MyDebug {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// E2.01 Boolean változó használata
+		//       Ha a debug változó értéke igaz, debug üzeneteket írjunk a konzolra
 	}
 
 }
