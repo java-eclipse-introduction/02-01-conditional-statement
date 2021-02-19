a39d192ba951d46107b1c8642eca30310f782653
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-19 12:51:36 +0100

    E1.01 Egyágú elágazás

--- 02-01-01-if-then-statement/src/org/example/MyIfThenStatement.java
+++ 02-01-01-if-then-statement/src/org/example/MyIfThenStatement.java
@@ -4,6 +4,8 @@ public class MyIfThenStatement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// E1.01 Egyágú elágazás
+		//		Írjon programot, amely megállapítja, hogy a megadott szám páros-e!
 
 	}
 
