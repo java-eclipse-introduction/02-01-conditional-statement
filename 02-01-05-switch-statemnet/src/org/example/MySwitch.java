447819ebea1f3fd3baaf6873d9965b3c06da62ef
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-19 13:52:49 +0100

    E5.02 Bekérjük, hogy a hét hanyaik napjáról van szó.

--- 02-01-05-switch-statemnet/src/org/example/MySwitch.java
+++ 02-01-05-switch-statemnet/src/org/example/MySwitch.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class MySwitch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		// E5.01 Írjon programot, amely megállapítja a hét napját!
+
+		// E5.02 Bekérjük, hogy a hét hanyaik napjáról van szó.
+		System.out.println("Megállapítom, a hét napját szövegesen!");
+		System.out.print("Adja meg, a hét hanyadik napjáról van szó: ");
+		Scanner input=new Scanner(System.in);
+		int dayNumber;
+		dayNumber=input.nextInt();
 	}
 
 }
