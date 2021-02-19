ccb774a265a25c391aa25955b5e8233205f5d5d2
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-19 13:06:59 +0100

    E3.01 Írjunk programot, amely megállpítja, hogy a szám páros vagy páratlan!

--- 02-01-03-if-then-else-statement/src/org/example/MyIfThenElseStatement.java
+++ 02-01-03-if-then-else-statement/src/org/example/MyIfThenElseStatement.java
@@ -5,6 +5,7 @@ public class MyIfThenElseStatement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// E3.01 Írjunk programot, amely megállpítja, hogy a szám páros vagy páratlan!
 	}
 
 }
