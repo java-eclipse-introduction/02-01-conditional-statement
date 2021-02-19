5d410d95aa7be12af375ea9e589aba5d29b07ccb
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-19 12:53:27 +0100

    E1.02 A feladatot kisebb részekre (szekvenciákra) bontjuk.

--- 02-01-01-if-then-statement/src/org/example/MyIfThenStatement.java
+++ 02-01-01-if-then-statement/src/org/example/MyIfThenStatement.java
@@ -6,7 +6,9 @@ public class MyIfThenStatement {
 		// TODO Auto-generated method stub
 		// E1.01 Egyágú elágazás
 		//		Írjon programot, amely megállapítja, hogy a megadott szám páros-e!
-
+		
+		// E1.02 A feladatot kisebb részekre (szekvenciákra) bontjuk.
+		//		 Megfogalmazzuk saját szavainkkal a kisebb részek algoritmusát. Ezt leprogramozzuk.
 	}
 
 }
