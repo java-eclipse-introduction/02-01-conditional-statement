23a151c0f727795e576a4795760a0fcd8c345cb8
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-03-08 15:51:45 +0100

    E4.02 Bekérjük a számot

--- 02-01-04-else-if-statement/src/org/example/MyElseIfStatemanet.java
+++ 02-01-04-else-if-statement/src/org/example/MyElseIfStatemanet.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class MyElseIfStatemanet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		// E4.01 Írjon programot, amely megállapítja, hogy a szám pozitív, negatív vagy nulla
+		
+		// E4.02 Bekérjük a számot
+		System.out.println("Megállapítom, hogy a szám pozitív, nulla, vagy negatív.");
+		System.out.print("Adja meg a számot: ");
+		Scanner input=new Scanner(System.in);
+		int number;
+		number=input.nextInt();
 	}
 
 }
